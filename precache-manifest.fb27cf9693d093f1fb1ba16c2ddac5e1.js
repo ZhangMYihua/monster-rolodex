99d771c2618dab8d553ec1a7d1798320173a58eb
Yihua Zhang <michaelzhang2@msn.com>
2019-05-04 19:03:38 +0200

    Updates

---
+++ precache-manifest.fb27cf9693d093f1fb1ba16c2ddac5e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0fca7240140309df495d7bee5515c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4da0184bc872d39a197",
+    "url": "/static/css/main.3a275c6b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "b4da0184bc872d39a197",
+    "url": "/static/js/main.5ee3c2d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
